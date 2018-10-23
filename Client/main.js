a6392cc595cdae485b73065ce9dab38860dc159e
Darkiring <abrahamarv@hotmail.es>
2018-10-23 16:09:04 -0400

    First commit

---
+++ Client/main.js
@@ -0,0 +1,31 @@
+var socket = io.connect('http://192.168.150.161:6677', { 'forceNew': true });
+
+socket.on('messages', (data) => {
+    console.log(data);
+    render(data);
+});
+
+render = (data) => {
+    var html = data.map((message, index) => {
+        return(`
+            <div class="message">
+                <strong>${message.nickname}</strong> dice:
+                <p>${message.text}</p>
+            </div>
+        `);
+    }).join(' ');
+    var div_msgs = document.getElementById('messages');
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
+}
+
+addMessage = (e) => {
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value,
+    };
+    document.getElementById('nickname').style.display = 'none';
+    document.getElementById('text').value = ' ';
+    socket.emit('add-message', message);
+    return false;
+}
